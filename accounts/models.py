6493bb45315f1e14b284fb818acab21ca76bb5ea
iammohammadhejazi <iammohammadhejazi@gmail.com>
2022-12-11 15:12:25 +0330

    user manager created and added to User model✌️

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
 from .validators import phone_number_validator
-# Create your models here.
+from .managers import UserManager
 
 # TODO: complate user model
 class User(AbstractBaseUser,PermissionsMixin):
     phone_number = models.CharField(max_length=11,validators=[phone_number_validator])
     is_active = models.BooleanField(default=True)
-
+    objects = UserManager()
     USERNAME_FIELD = "phone_number" 
     
     def __str__(self):
         return self.phone_number
-# TODO: write profile model 
+
+# DONE: write profile model 
 class Profile(models.Model):
     user = models.OneToOneField(User)
     first_name = models.CharField(max_length=128)
