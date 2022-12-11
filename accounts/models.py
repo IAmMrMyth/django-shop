1b73ec90cba15e9621beb069ac0e4d56633e53a6
iammohammadhejazi <iammohammadhejazi@gmail.com>
2022-12-11 15:03:29 +0330

    user - profile models create

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from .validators import phone_number_validator
 # Create your models here.
+
+# TODO: complate user model
+class User(AbstractBaseUser,PermissionsMixin):
+    phone_number = models.CharField(max_length=11,validators=[phone_number_validator])
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "phone_number" 
+    
+    def __str__(self):
+        return self.phone_number
+# TODO: write profile model 
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.user.phone_number}"
--- accounts/validators.py
+++ accounts/validators.py
@@ -3,5 +3,6 @@ from django.core.validators import RegexValidator
 
 phone_number_validator = RegexValidator(
     regex="^[0-9]{11}$",
-    message="شماره وارد شده اشتباه است."
+    message="شماره وارد شده اشتباه است.",
+    code="invalid_phone_number"
 )
