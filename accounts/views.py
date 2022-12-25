91b44f14d5fcc7f23100b82d0da4c2599f8da875
IAmMrMyth <iammrmyth@gmail.com>
2022-12-25 18:49:55 +0330

    add login logout view✌️

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import LoginForm
+from django.contrib.auth import (authenticate, login, logout)
+from django.contrib.auth.mixins import (LoginRequiredMixin)
 # Create your views here.
+
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "accounts/login.html", {})
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['phone_number'], password=data['password'])
+            if user != None:
+                login(request, user)
+
+                return redirect("/")
+            else:
+                return render(request, "accounts/login.html", {'error_messages': "phone number or password is incorrect"})
+
+        return render(request, "accounts/login.html", {'error_messages': form.errors})
+
+
+class LogoutView(LoginRequiredMixin,View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return redirect("/")
