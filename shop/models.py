16578db0d68ee7d2171b10a5fad48cfaddef543f
IAmMrMyth <iammrmyth@gmail.com>
2022-12-28 23:11:35 +0330

    create product - productimage - productfeature models

--- shop/models.py
+++ shop/models.py
@@ -18,3 +18,30 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    code = models.CharField(max_length=50)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="products/%Y/%m/%d/")
+
+    def __str__(self) -> str:
+        return self.product.name
+
+
+class ProductFeature(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    value = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.title}: {self.value} | {self.product.name}"
