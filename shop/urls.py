5d3e2d981087f3dd0d13c5f4c476aef24878bad4
IAmMrMyth <iammrmyth@gmail.com>
2022-12-29 17:47:00 +0330

    add product deatil views and urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView,ProductDetailView
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name="home")
+    path('', HomeView.as_view(), name="home"),
+    path('product/<pk>/', ProductDetailView.as_view(), name="product_detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import DetailView
+from .models import Product
 # Create your views here.
 
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'base.html', {})
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'shop/product-detail.html'
